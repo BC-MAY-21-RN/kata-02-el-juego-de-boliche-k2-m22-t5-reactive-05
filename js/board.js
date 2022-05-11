b8566fff3a26514e7147e2c161f86e8eaf5aeb0d
SaboHiriart <saboochoahiriart@outlook.com>
2022-05-11 17:39:45 -0500

    Methods of the game Co-authored-by: Deniz-Navarro <ldeniz1@ucol.mx> Co-authored-by: nicolasjrz <nicolasjrz.dev@gmail.com> Co-authored-by: Valentin <valeru.251@gmail.com>

--- js/board.js
+++ js/board.js
@@ -1,19 +1,71 @@
-import Round from "./Round";
+const Round = require("../js/round.js");
 
-export default class Board {
+class Board {
   constructor() {
-    this.gameBoard = new Array(10);
-    this.totalScore = 0;
-  }
-  
-  totalScore() {
-    return this.gameBoard.reduce((turno1, turno2) => turno1 + turno2);
+    this.gameBoard = new Array();
+    this.totalScore = new Array();
+    this.extraPointTenth = 0;
   }
 
-  game(){
-    for(let i=0; i < this.gameBoard.length(); i += 1){
+  game() {
+    for (let i = 0; i < 10; i += 1) {
       const round = new Round();
-      this.gameBoard.push(round)
+      round.runRound();
+      this.gameBoard.push(round);
     }
   }
+
+  setNewScores() {
+    let total = 0;
+    if (this.gameBoard[9].score === 10) {
+      this.extraPointTenth = Math.floor(Math.random() * 11);
+      this.gameBoard[9].score += this.extraPointTenth;
+    }
+
+    for (let i = 0; i < 9; i += 1) {
+      const tiroActual = this.gameBoard[i];
+      const tiroSiguiente = this.gameBoard[i + 1];
+      if (tiroActual.isStrike()) {
+        tiroActual.setScore(tiroSiguiente.score);
+      } else if (tiroActual.isSpare()) {
+        tiroActual.setScore(tiroSiguiente.getTiroUno());
+      }
+      total += tiroActual.score;
+      this.totalScore.push(total);
+    }
+    total += this.gameBoard[9].score;
+    this.totalScore.push(total);
+  }
+
+  printBoard() {
+    let cadena = "";
+    for (let i = 0; i < 10; i += 1) {
+      const ronda = this.gameBoard[i];
+        if(ronda.score<10){
+            cadena +=
+          "| " +
+          ronda.getTiroUno() +
+          " | " +
+          ronda.getTiroDos() +
+          " | ";
+          cadena += this.totalScore[i] +"\n";
+        }else{
+          if(ronda.isSpare()){
+            cadena +=
+          "| " +
+          ronda.getTiroUno() +
+          " | / | "
+          +  this.totalScore[i] + "\n";
+          }
+          if(ronda.isStrike()){
+            cadena +=
+            "|   | X | "
+            +  this.totalScore[i] + "\n";
+          }
+        }
+    }
+    console.log(cadena);
+  }
 }
+
+module.exports = Board;
