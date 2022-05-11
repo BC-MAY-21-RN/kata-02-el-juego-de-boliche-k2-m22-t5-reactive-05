714254bbff06dd029338db667624e178ca304f7b
SaboHiriart <saboochoahiriart@outlook.com>
2022-05-11 17:38:57 -0500

    New Methods of the object Co-authored-by: Deniz-Navarro <ldeniz1@ucol.mx> Co-authored-by: nicolasjrz <nicolasjrz.dev@gmail.com> Co-authored-by: Valentin <valeru.251@gmail.com>

--- js/round.js
+++ js/round.js
@@ -1,49 +1,70 @@
-export default class Round {
-    constructor () {
-        this.tiroUno = 0;
-        this.tiroDos = 0;
-        this.score = this.tiroUno + this.tiroDos;
-    }
+class Round {
+  constructor() {
+    this.tiroUno = 0;
+    this.tiroDos = 0;
+    this.score = 0;
+  }
 
-    get_tiroUno(){
-        return this.tiroUno;
-    }
-    
-    get_tiroDos(){
-        return this.tiroDos;
-    }
+  getTiroUno() {
+    return this.tiroUno;
+  }
 
-    is_spray(){
-        return  this.tiroUno + this.tiroDos === 10;
-    }
-    
-    is_strike(){
-        return this.tiroUno === 10;
-    }
+  getTiroDos() {
+    return this.tiroDos;
+  }
 
-    get_Round1() {
-        return Math.floor(Math.random() * 11);
-        
-      }
-    
-    get_Round2() {
-        const maxPinos = 10 -  this.tiroUno;
-        return Math.floor(Math.random() * maxPinos);
+  isSpare() {
+    if(this.tiroUno === 10){
+        return false;
+    }
+    return this.tiroUno + this.tiroDos ===10;
+  }
 
-      }
-      
-      run(){
-        const bonificacion1=10;
-        const bonificacion2=10;
-        this.tiroUno = this.get_Round1();
-        if(this.is_strike())
-        {
-          this.score += bonificacion1;
-        }else{
-            this.tiroDos = this.get_Round2();
-            if(this.is_spray()){
-                this.score += bonificacion2;
-            }
-        }
-      }
+  isStrike() {
+    return this.tiroUno === 10;
+  }
+
+  getRound1() {
+    return Math.floor(Math.random() * 11);
+  }
+
+  getRound2() {
+    const maxPinos = 11 - this.tiroUno;
+    return Math.floor(Math.random() * maxPinos);
+  }
+
+  runRound() {
+    this.tiroUno = this.getRound1();
+    this.tiroDos = this.getRound2();
+    this.score = this.tiroUno + this.tiroDos;
+  }
+
+  setScore(nextRoundScore){
+      this.score += nextRoundScore;
+  }
 }
+
+module.exports = Round;
+
+/**
+ * 
+ * primer ronda
+ *  3
+ *  7
+ *  / 18
+ * segundo ronda
+ *  8
+ *  2
+ *  / 12
+ * tercera ronda
+ * 2
+ * 4
+ * 6
+ * cuarta ronda
+ *  10
+ *  - 
+ *  X 18
+ * quinta ronda
+ * 5
+ * 3
+ */
